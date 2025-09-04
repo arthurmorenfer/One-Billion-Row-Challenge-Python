8bdc211101cd7176402672b768f98bd35c7445a1
arthurmorenfer <arthur.mfernandes@yahoo.com.br>
2025-09-03 22:10:08 -0300

    Finished the transformation from .txt (csv) to .parquet using duckdb, this process is basically getting all the files with .txt on the data folder in a list and them provide the file name to the function, the function will them concat the file_path, create the result file name and then process each file from the data folder, creating a parquet for each one. It took around 5-6 minutes to convert the billion rows file.

--- src/duckdb_parquet_transform.py
+++ src/duckdb_parquet_transform.py
@@ -1,16 +1,31 @@
-import duckdb
-
 def transform_files(measurement_file_name: str) -> str:
+    import duckdb
+    file_path = '../data/'
+    measurement_file_path_and_name = file_path + measurement_file_name 
     result = ""
-    measurement_file_name_result = measurement_file_name.split(".")[0]
+    measurement_file_name_result = str(measurement_file_name).split(".")[0]
     measurement_file_name_result = measurement_file_name_result + '.parquet'
     try:
-        duckdb.sql("""COPY
-                        (SELECT * FROM {measurement_file_name})
-                        TO '{measurement_file_name_result}'
+        duckdb.sql(f"""COPY
+                        (SELECT * FROM read_csv('{measurement_file_path_and_name}'))
+                        TO '{file_path}{measurement_file_name_result}'
                         (FORMAT parquet);""")
         result = "Success, file transformed."
         return result
     except ValueError as e:
         result = f"Failed, check the error: {e}"
         return result
+
+import os
+import glob
+from pathlib import Path
+
+root_folder = os.path.dirname(os.path.dirname(__file__))
+data_folder = root_folder + '/data'
+files_paths = glob.glob(os.path.join(data_folder,'*.txt'))
+
+for file in files_paths:
+    file_name = file.split("\\")[-1]
+    result_message = transform_files(file_name)
+    print(result_message)
+
